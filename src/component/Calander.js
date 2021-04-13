51551d20917e000b26b037d30554d77e1132a9e4
mahyarmadad <erfansam8891@yahoo.com>
2021-04-14 01:22:21 +0430

    add calander style

--- src/component/Calander.js
+++ src/component/Calander.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./Calander.scss";
+
 const days = Array.from({ length: 30 }, (_, i) => i + 1);
 
 export default function Calander() {
   return (
-    <div>
+    <div className="calander-dates">
       {days.map((day) => (
-        <div key={day}>{day}</div>
+        <div className="calander-date" key={day}>
+          <p>{day}</p>
+        </div>
       ))}
     </div>
   );
