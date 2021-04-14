be3c003433ae632fa0819102fcad51a13a500c35
mahyarmadad <erfansam8891@yahoo.com>
2021-04-14 21:08:23 +0430

    add redux to project

--- src/redux/addtodo-actions.js
+++ src/redux/addtodo-actions.js
@@ -2,3 +2,8 @@ export const addToDO = (task) => ({
   type: "ADD_TODO",
   payload: task,
 });
+
+export const makeItComplete = (id) => ({
+  type: "COMPLETE_TODO",
+  payload: id,
+});
--- src/redux/addtodo-reducer.js
+++ src/redux/addtodo-reducer.js
@@ -1,7 +1,18 @@
 export const addtodoReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return [...state, action.task];
+      return [...state, action.payload];
+    case "COMPLETE_TODO": {
+      return state.map((task, i) => {
+        if (i !== action.payload) {
+          return task;
+        }
+        return {
+          ...task,
+          complete: true,
+        };
+      });
+    }
     default:
       return state;
   }
